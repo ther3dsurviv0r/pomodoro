7640d848d08301800be4db31f2189f162a8d8dc8
Srijan Singh <52595391+ther3dsurviv0r@users.noreply.github.com>
2020-05-07 06:27:39 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,91 +1,128 @@
-//Author: ther3dsurviv0r
-var running = false;
-var on = false;
+var timerIsOn = false;
 var soundIsOn = true;
-var timer, audio;
-var setMin=25;
-function init(){
-    var second=document.getElementById("timer");
-    var min=document.getElementById("min");
-    if(!running){
-        second.innerHTML="59";
-	if(setMin<10){
-		min.innerHTML="0"+(setMin-1);
-    }else{
-		min.innerHTML=setMin-1;
-	}
-        running=true;
-    }else{
-        if(min.innerHTML<=0 && second.innerHTML<=0){
-            toggle();
-            if(soundIsOn){
-                audio = new Audio('sound/beep.mp3');
-                audio.play();
-            }
-        }else{
-            if(second.innerHTML>0){
-                second.innerHTML= parseInt(second.innerHTML) - 1;
-                if(second.innerHTML<10){
-                    second.innerHTML="0"+second.innerHTML;
+var timer,soundButton,placeMinute,placeSecond,timer25,timer5,active;
+
+
+class Time{
+    constructor(timerName,timerDurationMin,timerDurationSec,timerAudio,timerBackground){
+        this.name=timerName;
+        this.durationMin=timerDurationMin;
+        this.durationSec=timerDurationSec;
+        this.audio = new Audio(timerAudio);
+        this.background=timerBackground;
+    }
+
+    setParameters(){
+        placeMinute.innerHTML=this.timerSet(this.durationMin);
+        placeSecond.innerHTML=this.timerSet(this.durationSec);
+        document.body.style.backgroundColor=this.background;
+        active=this.name;
+    }
+
+    tick(){
+        if(parseInt(this.durationSec)<=0){
+            if(parseInt(this.durationMin)<=0){
+                stop();
+                if(soundIsOn){
+                    this.audio.play();
                 }
             }else{
-                second.innerHTML="59";
-                min.innerHTML= parseInt(min.innerHTML) - 1;
-                if(min.innerHTML<10){
-                    min.innerHTML="0"+min.innerHTML;
-                }
+                this.durationMin= parseInt(this.durationMin)-1;
+                this.durationSec=59;
             }
+        }else{
+            this.durationSec=parseInt(this.durationSec)-1;
         }
+        this.setParameters();
+    }
+
+    timerSet(value){
+        if(parseInt(value)<10){
+            return "0"+value;
+        }
+        return value;
     }
 }
 
-function reset(){
-    on=true;
-    toggle();
-    var second=document.getElementById("timer");
-    var min=document.getElementById("min");
-    running=false;
-    second.innerHTML="00";
-	if(setMin<10){
-		min.innerHTML="0"+setMin;
-    }else{
-		min.innerHTML=setMin;
-	}
+
+function init(){
+    placeSecond=document.getElementById("sec");
+    placeMinute=document.getElementById("min");
+	soundButton=document.getElementById("btnsound");
+    timer25 = new Time("Work",25,0,"sound/victory.mp3","tomato");
+    timer5 = new Time("Break",5,0,"sound/beep.mp3","cornflowerblue");
+    timer25.setParameters();
+    active=timer25.name;
 }
+
+
+function toggle(){
+    if(timerIsOn){
+        stop();
+    }else{
+        start();
+    }
+}
+
+
+function start(){
+    if(active==timer25.name){
+        timer = setInterval("timer25.tick();",1000);
+    }else if(active==timer5.name){
+        timer = setInterval("timer5.tick();",1000);
+    }else{
+        alert("Error!");
+    }
+    document.getElementById("btn").src = "image/pause.png";
+    timerIsOn=true;
+}
+
+
+function stop(){
+    clearInterval(timer);
+    document.getElementById("btn").src = "image/play.png";
+    timerIsOn=false;
+}
+
+
+function reset(){
+    stop();
+    timer25.durationMin=25;
+    timer25.durationSec=0;
+    timer5.durationMin=5;
+    timer5.durationSec=0;
+    if(active==timer25.name){
+        timer25.setParameters();
+    }else if(active==timer5.name){
+        timer5.setParameters();
+    }else{
+        alert("Error!");
+    }
+}
+
+
+function changeTime(){
+    switch(active){
+        case timer25.name:
+            timer5.setParameters();
+            reset();
+            break;
+        case timer5.name:
+            timer25.setParameters();
+            reset();
+            break;
+        default:
+            alert("Reset the timer and try again.");
+    }
+}
+
 
 function soundToggle(){
     if(soundIsOn){
-        document.getElementById("btnsound").src = "image/soundoff.png";
+        soundButton.src = "image/soundoff.png";
         soundIsOn=false;
     }else{
-        document.getElementById("btnsound").src = "image/soundon.png";
+        soundButton.src = "image/soundon.png";
         soundIsOn=true;
     }
-}
-
-function toggle(){
-    if(!on){
-        timer = setInterval("init();",1000);
-        document.getElementById("btn").src = "image/pause.png";
-        on=true;
-    }else{
-        clearInterval(timer);
-        document.getElementById("btn").src = "image/play.png";    
-        on=false;   
-    }
-}
-
-function changeTime(){
-    if(document.getElementById("min").innerHTML==25){
-        setMin=5;
-        document.body.style.backgroundColor="cornflowerblue";
-        reset();
-    }else if(document.getElementById("min").innerHTML==5){
-        setMin=25;
-        document.body.style.backgroundColor="tomato";
-        reset();
-    }else if(document.getElementById("min").innerHTML==0){
-		reset();
-		changeTime();
-	}
 }
---
+++ script/script_old.js
@@ -0,0 +1,91 @@
+//Author: ther3dsurviv0r
+var running = false;
+var on = false;
+var soundIsOn = true;
+var timer, audio;
+var setMin=25;
+function init(){
+    var second=document.getElementById("sec");
+    var min=document.getElementById("min");
+    if(!running){
+        second.innerHTML="59";
+	if(setMin<10){
+		min.innerHTML="0"+(setMin-1);
+    }else{
+		min.innerHTML=setMin-1;
+	}
+        running=true;
+    }else{
+        if(min.innerHTML<=0 && second.innerHTML<=0){
+            toggle();
+            if(soundIsOn){
+                audio = new Audio('sound/beep.mp3');
+                audio.play();
+            }
+        }else{
+            if(second.innerHTML>0){
+                second.innerHTML= parseInt(second.innerHTML) - 1;
+                if(second.innerHTML<10){
+                    second.innerHTML="0"+second.innerHTML;
+                }
+            }else{
+                second.innerHTML="59";
+                min.innerHTML= parseInt(min.innerHTML) - 1;
+                if(min.innerHTML<10){
+                    min.innerHTML="0"+min.innerHTML;
+                }
+            }
+        }
+    }
+}
+
+function reset(){
+    on=true;
+    toggle();
+    var second=document.getElementById("sec");
+    var min=document.getElementById("min");
+    running=false;
+    second.innerHTML="00";
+	if(setMin<10){
+		min.innerHTML="0"+setMin;
+    }else{
+		min.innerHTML=setMin;
+	}
+}
+
+function soundToggle(){
+    if(soundIsOn){
+        document.getElementById("btnsound").src = "image/soundoff.png";
+        soundIsOn=false;
+    }else{
+        document.getElementById("btnsound").src = "image/soundon.png";
+        soundIsOn=true;
+    }
+}
+
+function toggle(){
+    if(!on){
+        timer = setInterval("init();",1000);
+        document.getElementById("btn").src = "image/pause.png";
+        on=true;
+    }else{
+        clearInterval(timer);
+        document.getElementById("btn").src = "image/play.png";    
+        on=false;   
+    }
+}
+
+function changeTime(){
+    if(document.getElementById("min").innerHTML==25){
+        setMin=5;
+        document.body.style.backgroundColor="cornflowerblue";
+        reset();
+    }else if(document.getElementById("min").innerHTML==5){
+        setMin=25;
+        document.body.style.backgroundColor="tomato";
+        reset();
+    }else if(document.getElementById("min").innerHTML==0){
+		reset();
+		changeTime();
+	}
+}
